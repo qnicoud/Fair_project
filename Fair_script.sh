eee44f04ada0076339b45e40d92f5f65284c58f8
root <root@DESKTOP-F39E0PL.localdomain>
2019-02-26 11:21:10 +0100

    Initialize Fair_script.sh - Architecture

---
+++ Fair_script.sh
@@ -0,0 +1,10 @@
+mkdir Project
+mkdir Project/samples
+mkdir Project/annotations
+mkdir Project/bowtie2
+mkdir Project/fastqc
+mkdir Project/genome
+mkdir Project/graphics
+mkdir Project/htseq
+mkdir Project/reference
+mkdir Project/samtools
