4fb0c24e5a533d4c0b2e0bc2e74c5b106d0003a2
root <root@DESKTOP-F39E0PL.localdomain>
2019-02-27 15:38:11 +0100

    Edit FAIR_script.sh to do the hole structure of the folder and the download of SRA files

--- Fair_script.sh
+++ Fair_script.sh
@@ -1,3 +1,19 @@
+##--------------------------------------------------------------------------------------------------------------------
+## FAIR script
+## Author: T. Denecker & C. Toffano-Nioche
+## affiliation: I2BC
+## Aim: A workflow to process RNAseq.
+## Organism: O.tauri
+## Date: Jan 2019
+## Step:
+##	1- Create tree structure
+##	2- Sownload data from SRA
+##--------------------------------------------------------------------------------------------------------------------
+
+echo "================================================================================"
+echo "Creation of tree structure"
+echo "================================================================================"
+
 mkdir Project
 mkdir Project/samples
 mkdir Project/annotations
@@ -8,3 +24,42 @@ mkdir Project/graphics
 mkdir Project/htseq
 mkdir Project/reference
 mkdir Project/samtools
+
+echo "================================================================================"
+echo "Download data from SRA"
+echo "================================================================================"
+
+cd Project/samples
+
+IFS=$'\n'		# Make newlines the only separator
+
+for j in $(tail -n +2 ../../PRJNA304086true.txt)
+do
+	# get important informations from the line
+	access = $(echo "$j" | cut -f6)
+	id = $(echo "$j" | cut -f1)
+	md5 = $(echo "$j" | cut -f7)
+
+	echo "-----------------------------------------------------------------------"
+	echo ${id}
+	echo "-----------------------------------------------------------------------"
+
+	# Download file
+	wget ${acces}
+
+	# Get md5 of downloaded file
+	md5_local = "$(md5sum $id.fastq.gz | cut -d' ' -f1)"
+	echo $md_local
+
+	# Test md5
+	if [ "$md5_local" == "$md5" ]
+	then
+		echo "Done"
+	else
+		echo "Nope"
+		exit
+	fi
+done
+
+cd ../..
+
